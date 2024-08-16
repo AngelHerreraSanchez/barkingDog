05ac5ffae9e7be9071120abdc80afd9527f5f8cf
herreang000 <herreang000@warren.k12.in.us>
2024-08-16 11:33:21 -0400

    final

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        boolean result = shouldWakeUp(true,4);
+        System.out.println(result);
+
+    }
+
+    public static boolean shouldWakeUp(boolean barking, int hourOfTheDay){
+        if (hourOfTheDay < 0 && hourOfTheDay > 23) {
+            return false;
+        } else if( hourOfTheDay < 8 && hourOfTheDay > 22){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+
 }
